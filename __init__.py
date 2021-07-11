37a77a5792b3106e2efe10007002454141d45246
MURATAGAWA Kei <muratagawa@gmail.com>
2021-07-11 13:36:51 +0900

    Update bl_info

--- __init__.py
+++ __init__.py
@@ -9,15 +9,15 @@ bl_info = {
     "version": (0, 0, 2),
     "location": "Material Properties",
     "warning": "",
-    "category": "Material"
-    # "wiki_url": "https://github.com/muratagawa/dimensions-per-keyframe/",
-    # "tracker_url": "https://github.com/muratagawa/dimensions-per-keyframe/issues",
+    "category": "Material",
+    "wiki_url": "https://github.com/muratagawa/material_quick_override/",
+    "tracker_url": "https://github.com/muratagawa/material_quick_override/issues",
 }
 
 
-class MQO_OT_override(Operator):
-    bl_idname = "object.material_quick_override"
-    bl_label = "Override Material"
+class MQO_OT_override_single(Operator):
+    bl_idname = "object.material_quick_override_single"
+    bl_label = "Active Only"
     bl_description = "Override active material to be unique."
     bl_options = {'REGISTER', 'UNDO'}
 
@@ -37,6 +37,29 @@ class MQO_OT_override(Operator):
         return {'FINISHED'}
 
 
+# TODO
+# class MQO_OT_override_all(Operator):
+#     bl_idname = "object.material_quick_override_all"
+#     bl_label = "All"
+#     bl_description = "Override all materials."
+#     bl_options = {'REGISTER', 'UNDO'}
+
+#     def execute(self, context):
+#         index = context.object.active_material_index
+
+#         # Skip if not library linked material
+#         if context.object.material_slots[index].material.library is None:
+#             self.report({'INFO'}, "This material is not linked with library override. Skipped.")
+#             return {'FINISHED'}
+
+#         new_mat = context.object.active_material.copy()
+#         context.object.material_slots[index].link = 'OBJECT'
+#         context.object.material_slots[index].material = new_mat
+
+#         self.report({'INFO'}, "Material overwritten.")
+#         return {'FINISHED'}
+
+
 class MQO_PT_override(Panel):
     """Creates a Panel in the Material properties window"""
     bl_label = "Material Quick Override"
@@ -47,12 +70,13 @@ class MQO_PT_override(Panel):
     bl_parent_id = "EEVEE_MATERIAL_PT_context_material"
 
     def draw(self, context):
-        self.layout.operator(MQO_OT_override.bl_idname)
+        self.layout.operator(MQO_OT_override_single.bl_idname)
 
 
 classes = (
     MQO_PT_override,
-    MQO_OT_override
+    MQO_OT_override_single,
+    # MQO_OT_override_all
 )
 
 
